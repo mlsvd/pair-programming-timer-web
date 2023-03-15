abd2407f2413ac9607e82721eeccfadb321f92ab
Dmitri Malisev <malisev.dmitri@gmail.com>
2023-03-15 18:39:46 +0100

    Add timing and show hide controls

--- app/src/components/Timer.js
+++ app/src/components/Timer.js
@@ -6,44 +6,101 @@ class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isRunning: false,
-            timeSelected: 60,
-            currentSecond: 60,
+            status: 'default',
+            timeSelected: 5,
+            currentSecond: 5,
+            timerDisplayValue: '--:--',
+            intervalRef: null
         }
     }
     startTimer() {
-        this.setState({isRunning: true});
+        this.setState({status: 'started'});
+        this.startTicking();
     }
     pauseTimer() {
-        this.setState({isRunning: false});
+        this.setState({status: 'paused'});
     }
     resetTimer() {
-        this.setState({isRunning: false});
+        this.setState({status: 'default'});
+        this.stopTicking();
+        this.setState({currentSecond: 5});
+        this.setState({timerDisplayValue: '--:--'});
+
     }
     continueTimer() {
-        this.setState({isRunning: true});
+        this.setState({status: 'started'});
+        this.startTicking();
+    }
+
+    startTicking() {
+        this.stopTicking();
+        this.tick();
+        var tickerIntervalValue = setInterval(
+            function() {
+                this.tick();
+            }.bind(this),
+            1000
+        );
+        this.setState({intervalRef: tickerIntervalValue});
+    }
+    stopTicking() {
+        clearInterval(this.state.intervalRef);
     }
     tick() {
-        if (!this.state.isRunning) {
+        if (this.state.status !== 'started') {
             return;
         }
 
-        this.setState({currentSecond: this.state.currentSecond--});
+        if (this.state.currentSecond <= 0) {
+            this.stopTicking();
+            this.setState({status: 'finished'});
+            return;
+        }
+
+        this.setState({currentSecond: --this.state.currentSecond});
         if (this.state.currentSecond <= 0) {
             this.stopTicking();
         }
 
-
+        this.setState({timerDisplayValue: this.timeToDisplayValue()})
     }
+    timeToDisplayValue() {
+        var timeDTO = this.transformSecondsToTimeDTO(this.state.currentSecond);
+        var valuesArray = [];
+        if (timeDTO.hours > 0) {
+            valuesArray.push(timeDTO.hours);
+        }
+        valuesArray.push(timeDTO.minutes);
+        valuesArray.push(timeDTO.seconds);
+
+        return valuesArray.join(":")
+    }
+    transformSecondsToTimeDTO(totalSeconds) {
+        var seconds = Math.floor(totalSeconds % 60);
+        totalSeconds /= 60;
+        var minutes = Math.floor(totalSeconds % 60);
+        totalSeconds /= 60;
+        var hours = Math.floor(totalSeconds % 24);
+        totalSeconds /= 24;
+        var days = Math.floor(totalSeconds);
+
+        return {
+            "days" :  ("0" + days).slice(-2),
+            "hours" : ("0" + hours).slice(-2),
+            "minutes" : ("0" + minutes).slice(-2),
+            "seconds" : ("0" + seconds).slice(-2)
+        };
+    }
+
     render() {
         return (
             <div>
-                <TimerDisplay />
-                <TimerControls
+                <TimerDisplay timerDisplayValue={this.state.timerDisplayValue} />
+                <TimerControls status={this.state.status}
                     onStartTimer={() => this.startTimer()}
                     onPauseTimer={() => this.pauseTimer()}
-                    onResetTimer={() => this.continueTimer()}
-                    onContinueTimer={() => this.resetTimer()}
+                    onResetTimer={() => this.resetTimer()}
+                    onContinueTimer={() => this.continueTimer()}
                 />
             </div>
         );
--- app/src/components/TimerControls.js
+++ app/src/components/TimerControls.js
@@ -5,12 +5,35 @@ class TimerControls extends React.Component {
         console.log(1);
     }
     render() {
+        if (this.props.status === 'paused') {
+            return (
+                <p className="lead">
+                    <button id="continue-button" onClick={() => this.props.onContinueTimer()} className="timer-button play-btn hidden"><span>Continue</span></button>
+                    <button id="reset-button" onClick={() => this.props.onResetTimer()} className="timer-button stop-btn play-btn hidden"><span>Reset</span></button>
+                </p>
+            );
+        }
+
+        if (this.props.status === 'started') {
+            return (
+                <p className="lead">
+                    <button id="pause-button" onClick={() => this.props.onPauseTimer()} className="timer-button pause-btn hidden"><span>Pause</span></button>
+                    <button id="reset-button" onClick={() => this.props.onResetTimer()} className="timer-button stop-btn play-btn hidden"><span>Reset</span></button>
+                </p>
+            );
+        }
+
+        if (this.props.status === 'finished') {
+            return (
+                <p className="lead">
+                    <button id="reset-button" onClick={() => this.props.onResetTimer()} className="timer-button stop-btn play-btn hidden"><span>Reset</span></button>
+                </p>
+            );
+        }
+
         return (
             <p className="lead">
                 <button id="start-button" onClick={() => this.props.onStartTimer()} className="timer-button play-btn"><span>Start</span></button>
-                <button id="pause-button" onClick={() => this.props.onPauseTimer()} className="timer-button pause-btn hidden"><span>Pause</span></button>
-                <button id="continue-button" onClick={() => this.props.onContinueTimer()} className="timer-button play-btn hidden"><span>Continue</span></button>
-                <button id="reset-button" onClick={() => this.props.onResetTimer()} className="timer-button stop-btn play-btn hidden"><span>Reset</span></button>
             </p>
         );
     }
