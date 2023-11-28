fe29740321520b687f91025e210046a7a16c3110
Dmitri Malisev <malisev.dmitri@gmail.com>
2023-11-28 09:21:43 +0100

    Add more functionality

--- app/src/components/AboutPage.js
+++ app/src/components/AboutPage.js
@@ -14,10 +14,9 @@ class AboutPage extends React.Component {
                       <h2>Pair Programming Timekeeper</h2>
                       <span>The app offers flexible time intervals catering to the dynamic nature of pair programming sessions.</span>
                       <span>Users can choose from intervals of 1 minute, 10 minutes, 50 minutes, and 1 hour to structure their collaborative coding sessions.</span>
-                      <span>The timer runs discreetly in the background, allowing team members to focus on their work while staying mindful of time.</span>
                       <br />
                       <span>Pair programming sessions often benefit from periodic breaks to maintain productivity and engagement.</span>
-                      <span>The app's intuitive design ensures that teams can seamlessly switch roles between the driver and navigator, fostering a balanced and effective collaboration rhythm.</span>
+                      <span>The app helps teams to switch roles between the driver and navigator, fostering a balanced and effective collaboration rhythm.</span>
                   </div>
                 <br />
                 <hr />
--- app/src/components/Title.js
+++ app/src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 class Title extends React.Component {
     render () {
         return (
-            <h1>&#9202; Pair Programming Timekeeper</h1>
+            <h1>Pair Programming Timekeeper</h1>
         );
     }
 }
